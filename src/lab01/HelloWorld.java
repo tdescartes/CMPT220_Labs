6214c39bbdd0b11f1f2f071fb532db0714e96fdb
Your Name <descartestuyishime@148-100-133-65.FoxNet.marist.edu>
2020-01-30 14:37:33 -0500

    Hello World Program!

---
+++ src/lab01/HelloWorld.java
@@ -0,0 +1,13 @@
+package lab01;
+
+public class HelloWorld {
+
+    /**
+     * Obligatory starter code.
+     * @param args command line arguments.
+     */
+
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+    }
+}
