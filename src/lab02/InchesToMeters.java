7873a95a605ef33bf025c85fdf4914d074792542
Your Name <descartestuyishime@148-100-136-101.FoxNet.marist.edu>
2020-02-08 00:00:41 -0500

    The second lab, Inchestometer and Temperature converter

--- src/lab02/InchesToMeters.java
+++ src/lab02/InchesToMeters.java
@@ -1,4 +1,27 @@
 package lab02;
 
-public class InchesToMeters {
-}
+import java.util.Scanner;
+
+    public class InchesToMeters {
+
+
+        public static void main(String[] args) {
+
+            // Importing
+            Scanner input = new Scanner(System.in);
+
+            // assign the variable METERS_PER_INCH  with a conversion rate from inches to meters.
+            double METERS_PER_INCH = 0.0254;
+
+            // requesting a number to be converted from the user.
+            System.out.println("Enter a number in inches");
+            double inches = input.nextDouble();
+
+
+            // creating the outputs for the calculated/converted measurement.
+            System.out.println(inches + " inches are  equivalent to "+ (inches * METERS_PER_INCH)+ " meters");
+
+            }
+        }
+
+
--- src/lab02/TemperatureConverter.java
+++ src/lab02/TemperatureConverter.java
@@ -1,5 +1,42 @@
 package lab02;
+import java.util.Scanner;
+
 
 public class TemperatureConverter {
-    //
+
+    public static void main(String[] args) {
+
+        // Importing scanner
+        Scanner input = new Scanner(System.in);
+
+        // requesting a number to be converted from the user.
+        System.out.println("Enter degrees to be converted: ");
+        String degrees = input.next();
+
+        // splitting and getting the last letter that correspond to the measurements
+        char metrics = degrees.charAt(degrees.length()-1);
+
+
+        // splitting and getting the numerical value of the string.
+        degrees= (degrees.substring(0, degrees.length()-1));
+
+        // converting string to double
+        double degree = Double.parseDouble(degrees);
+
+        // If the degrees are entered in Celcius degrees.
+        if (metrics == 'C') {
+            // the output afterward.
+            System.out.println( degrees + " Celcius degrees is equivalent " + ( 1.8*(degree) + 32) +" Farheneit degrees");
+        }
+
+        // If the degrees are entered in farheneit
+        else if ( metrics == 'F') {
+        // the output afterward.
+            System.out.println( degrees + " Farheneit degrees is equivalent " + ( 5*(degree - 32)/9) + " Celcius degrees");
+        }
+
+
+    }
 }
+
+
