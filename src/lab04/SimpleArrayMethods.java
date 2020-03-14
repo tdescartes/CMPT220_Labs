c3016bdf4e53e1aea86ec11ff278863065aca972
Your Name <descartestuyishime@148-100-186-254.FoxNet.marist.edu>
2020-03-14 12:54:52 -0400

    The lab 04, which contains SimpleArrayMethod and TicTacToe

--- src/lab04/SimpleArrayMethods.java
+++ src/lab04/SimpleArrayMethods.java
@@ -1,8 +1,74 @@
 package lab04;
+// The program with various methods tha
+//Descartes Tuyishime
+// February March 01, 2020
 
 public class SimpleArrayMethods {
+    public static void main(String args[]) {
+        int[] integers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        // The output from the methods (the methods are called with 'integers' as an argument)
+        System.out.println("The number of even integers in the array is "+ CountEven(integers));
+        System.out.println("The number of integers above average is " + CountAboveAverage(integers));
+        System.out.println("The number of positive integers is " + CountPositiveNumbers(integers));
+        System.out.println("The biggest integer from the array " + getLargest(integers));
+        System.out.println("The smallest integer from the array " + getSmallest(integers));
+    }
 
+    // the method for counting even numbers
+    public static int CountEven(int[] integers) {
+        int counting=0;
+        for (int index : integers) {
+            // A number modulo 2 with no remainder is even
+            if(index% 2== 0 )
+            counting+= 1;
+        }
+        return counting;
+    }
 
+    // the method that counts the numbers above the average in the array.
+    public static int CountAboveAverage(int[] integers) {
+        int sum=0, abcount=0; // declaration of variables needed for incrementation
+        for (int index : integers) {
+            sum+=index;
+        }
+       double average= sum / integers.length;
+        for (int index : integers) {
+            if (index > average)
+            abcount+= 1 ;
+        }
+        return abcount;// returning a number of integers that is that greater average
+    }
 
+    //the method that counts the positive numbers in the array
+    public static int CountPositiveNumbers(int[] integers) {
+        int countPosivitenumber=0;
+        for (int index : integers) {
+            if(index > 0 )
+                countPosivitenumber+= 1;
+        }
+        return countPosivitenumber;
+    }
+
+    // the method that compares and get the largest number in the array
+    public static int getLargest(int[] integers) {
+        int largest= integers[0];
+        for (int index : integers) {
+            if(largest < index){
+                largest= index;
+            }
+        }
+        return largest;
+    }
+
+    //the method that compares and get the least number in the array
+    public static int getSmallest(int[] integers) {
+        int smallest= integers[0];
+        for (int index : integers) {
+            if (smallest > index) {
+                smallest = index;
+            }
+        }
+        return smallest;
+    }
 
 }
--- src/lab04/TicTacToe.java
+++ src/lab04/TicTacToe.java
@@ -2,11 +2,85 @@ package lab04;
 import java.util.Scanner;
 
 public class TicTacToe {
-        /** Create Scanner to read input from */
-
+        /** Create Scanner to read input from **/
         private static final Scanner scanner = new Scanner(System.in);
 
+        /**
+         * Entry point to our game.
+         *
+         * @param args commandline arguments.
+         */
 
+       // symbols used in the games.
+       private static final String[] symbol = {" ", "x", "o"};
 
+        public static void main(String[] args) {
+               int[][] board = {{0,0,0},{0,0,0},{0,0,0}};
 
+                // Initialize player
+                int player = 1;
+
+                // Main game loop:
+                while (hasbanks(board)) {
+                        //Display game state.
+                        printBoard(board);
+
+                        // Get player move.
+                        int[] coordinates = getPlayerMove();
+                        int row = coordinates[0];
+                        int col = coordinates[1];
+
+                        //Mark the specified spaces.
+                        markBoard(board, row, col, player);
+
+                        // Switch player for next turn.
+                        player = (player % 2) + 1;
+                }
+        }
+
+        //method to access the col,row, and the board
+        private static boolean hasbanks(int[][] board) {
+            boolean flag = false;
+            for (int[] row : board) {
+                for (int square : row) {
+                    if (square == 0)
+                        flag= true;
+
+                }
+            }
+                return flag;
+
+        }
+
+        // method to print the board
+        private static void printBoard (int[][] board){
+            System.out.println("+-----------+");
+            for(int[] row: board){
+                String rowStr= "| ";
+                for(int square: row){
+                    rowStr= rowStr + symbol[square] + " | ";
+                }
+                System.out.println(rowStr);
+
+                System.out.println("+-----------+");
+            }
+
+        }
+
+        // Method to prompt the user to input their moves
+        private static int[] getPlayerMove(){
+            System.out.println("Enter the row number: ");
+            int row = scanner.nextInt();
+            System.out.println("Enter the column number: ");
+            int col =scanner.nextInt();
+
+            return new int[] {row, col};
+        }
+
+        // Method to assign the player's move to the board of the game
+        private static void markBoard (int[][] board, int row, int col, int player ){
+            if(board[row][col] == 0){
+                board[row][col] = player;
+            }
+        }
 }
